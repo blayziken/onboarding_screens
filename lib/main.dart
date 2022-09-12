b6ff8d5d1e17d321d5894d6f3eb0952dc17ebdfc
blayziken <kingtoluwalope@gmail.com>
2022-09-12 12:20:31 +0100

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,8 @@ import 'WhiteFang/home.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // Widget? currentScreen = screens['MarcusInspired'];
   @override
   Widget build(BuildContext context) {
-    // currentScreen == WhiteFang()
-    //     ? SystemChrome.setSystemUIOverlayStyle(
-    //         SystemUiOverlayStyle(
-    //           statusBarColor: Colors.transparent,
-    //           statusBarIconBrightness: Brightness.dark,
-    //           statusBarBrightness: Platform.isAndroid ? Brightness.dark : Brightness.light,
-    //           systemNavigationBarColor: Colors.white,
-    //           systemNavigationBarDividerColor: Colors.grey,
-    //           systemNavigationBarIconBrightness: Brightness.dark,
-    //         ),
-    //       )
-    //     : null;
     return MaterialApp(
       title: 'Onboarding UIs',
       debugShowCheckedModeBanner: false,
